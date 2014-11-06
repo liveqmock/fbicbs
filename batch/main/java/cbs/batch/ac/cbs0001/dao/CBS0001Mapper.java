d61a905fd59f90a7f6f66fe5b91e1e03665e1d74
wanglichao <wanglichao@163.com>
2014-11-06 17:00:35 +0800

    如果没有传票，给sum赋值为0

--- batch/main/java/cbs/batch/ac/cbs0001/dao/CBS0001Mapper.java
+++ batch/main/java/cbs/batch/ac/cbs0001/dao/CBS0001Mapper.java
@@ -4,6 +4,6 @@ import org.apache.ibatis.annotations.Select;
 
 public interface CBS0001Mapper {
 
-    @Select("SELECT SUM(TXNAMT) FROM ACTVCH WHERE RECSTS=' '")
+    @Select("SELECT nvl(SUM(TXNAMT),0) FROM ACTVCH WHERE RECSTS=' '")
     String qrySumAmt();
 }
