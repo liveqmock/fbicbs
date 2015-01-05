e508fcbbd3f3e080bc2029429b2b58e68e731fc0
wanglichao <wanglichao@163.com>
2015-01-05 10:18:59 +0800

    历史对账单打印

--- servlets/main/java/cbs/view/batch/Act0003HisPrtAction.java
+++ servlets/main/java/cbs/view/batch/Act0003HisPrtAction.java
@@ -48,11 +48,21 @@ import java.util.*;
 @ManagedBean
 @ViewScoped
 public class Act0003HisPrtAction implements Serializable {
-    private static final Log logger = LogFactory.getLog(ActvchPrtAction.class);
+    private static final Log logger = LogFactory.getLog(Act0003HisPrtAction.class);
 
     @ManagedProperty("#{ibatisManager}")
     IbatisManager ibatisManager;
 
+    String curnmc = "";
+    String actnam = "";
+    String glcnam = "";
+    String depnam = "";
+    String actacn = "";
+    String sysidt = "";
+    String cusidt = "";
+    String apcode = "";
+    String curcde = "";
+    String irtval = "";
     private ActstmVO vo = new ActstmVO();
     private ActnoName act = new ActnoName();
     private ActstmhisModel selectedRecord;
@@ -68,6 +78,25 @@ public class Act0003HisPrtAction implements Serializable {
                     vo.getAccode(), null);
             if (stmList == null || stmList.size() == 0) {
                 MessageUtil.addInfoWithClientID("msgs", "M546");
+            }
+            ActstmMapper stmmap = session.getMapper(ActstmMapper.class);
+            List<ActstmForActnum> stmnumLst = stmmap.selectForActnum2(vo.getAccode().trim());
+            ActstmForActnum stmnum = stmnumLst.get(0);
+            curnmc = stmnum.getCurnmc();
+            actnam = stmnum.getActnam();
+            glcnam = stmnum.getGlcnam();
+            depnam = stmnum.getDepnam();
+            actacn = stmnum.getOldacn();
+            sysidt = stmnum.getSysidt();
+            cusidt = stmnum.getCusidt();
+            apcode = stmnum.getApcode();
+            curcde = stmnum.getCurcde();
+            irtval = "";
+            DecimalFormat dformt = new DecimalFormat("0.0000");
+            if (stmnum.getIrtval() == null) {
+                irtval = "0 ‰";
+            } else {
+                irtval = dformt.format(stmnum.getIrtval().doubleValue() / 12 * 10) + " ‰";
             }
             setTableToFirstPage("resultForm:resultDataTable");
         } catch (Exception e) {
@@ -87,35 +116,112 @@ public class Act0003HisPrtAction implements Serializable {
             return;
         }
         FacesContext ctx = FacesContext.getCurrentInstance();
-        HttpServletResponse response = (HttpServletResponse) ctx.getExternalContext().getResponse();
+        HttpServletResponse resp = (HttpServletResponse) ctx.getExternalContext().getResponse();
+        String reportdate = SystemService.getBizDate10();//出账日业务日期
         OperatorManager om = OnlineService.getOperatorManager();
         String deptid = om.getOperator().getDeptid();
         String orgnam = SystemService.getDepnamByDeptid(deptid);
         try {
-            String title = (orgnam == null) ? "对账单流水账" : orgnam + "对账单流水账";
-            float[] widths = {140f, 140f, 140f, 100f, 140f, 140f, 140f};// 设置表格的列以及列宽
-            String[] columns = new String[]{"记账日期", "凭证号码", "摘要", "借贷别", "借方", "贷方", "交易后余额"};
-            String[] fields = new String[]{"stmdat", "furinf", "thrref", "borlen", "boramt", "lenamt", "actbal"};
-            int[] aligns = new int[]{Element.ALIGN_CENTER, Element.ALIGN_LEFT, Element.ALIGN_LEFT, Element.ALIGN_CENTER,
-                    Element.ALIGN_RIGHT, Element.ALIGN_RIGHT, Element.ALIGN_RIGHT};
-            String qryDate = vo.getBgndat() + "至" + vo.getEnddat();
-            String actmsg = "账号:" + act.getAccode() + " 单位:" + act.getActnam();
-            String strDate = "记账日期:" + qryDate;
-            String[] afterTitleMsgs = new String[]{actmsg, strDate};
-            int[] msgAligns = new int[]{Element.ALIGN_LEFT, Element.ALIGN_RIGHT};
-            String[] lastMsgs = new String[]{"", "交易记录数:" + stmList.size()};
-            // "打印时间:"+new SimpleDateFormat("yyyy-MM-dd HH:mm").format(new Date())+"  "};
-            Rectangle pageSize = new Rectangle(241 * 72 / 25.4f, 250 * 72 / 25.4f);
-            // Rectangle pageSize = PageSize.CROWN_QUARTO;
-            PdfReportFile pdfReport = new PdfReportFile(title, afterTitleMsgs, msgAligns, columns, fields, aligns, pageSize, widths, stmList, lastMsgs);
-            ByteArrayOutputStream ops = pdfReport.generatePdfDataStream();
-            response.reset();
-            ServletOutputStream out = response.getOutputStream();
-            response.setContentType("application/pdf");
-            response.setHeader("Content-disposition", "inline");
-            response.setContentLength(ops.size());
-            response.setHeader("Cache-Control", "max-age=30");
-            ops.writeTo(out);
+            int count = stmList.size(); //  总行数
+            int pageCount = 27;       // 每页记录数
+            int page = 0;             // 总共页数
+            /** 主要控制总共的页数*/
+            if (count >= pageCount && count % pageCount == 0) {
+                page = count / pageCount;
+            } else {
+                page = count / pageCount + 1;
+            }
+            PdfReader reader = null;
+            Document document = new Document();
+            ByteArrayOutputStream bao = new ByteArrayOutputStream();
+            PdfCopy pdfCopy = new PdfCopy(document, bao);
+            DecimalFormat df0 = new DecimalFormat("###,##0.00");
+            ByteArrayOutputStream baos[] = new ByteArrayOutputStream[page];
+            for (int item = 0; item < page; item++) {
+                InputStream isr = FacesContext.class.getResourceAsStream("/../../batchTmp/rp0003.pdf");
+                baos[item] = new ByteArrayOutputStream();
+                reader = new PdfReader(isr);
+                PdfStamper ps = new PdfStamper(reader, baos[item]);
+                AcroFields fields = ps.getAcroFields();
+                fields.setField("curnmc", curnmc);   //货币
+                fields.setField("glcnam", glcnam);   //科目
+                fields.setField("orgnam", orgnam);   //机构
+                fields.setField("depnam", depnam);   //部门
+                fields.setField("actnam", act.getActnam());   //户名
+                fields.setField("actacn", act.getAccode());   //账号
+                fields.setField("irtval", irtval);   //利率：月
+                fields.setField("dat", reportdate);   //出账日
+                int i = 0;
+                int from = item * pageCount;
+                int to = from + pageCount;
+                if (to > count) {
+                    to = count;
+                }
+                for (ActstmhisModel stm : stmList.subList(from, to)) {
+                    for (int j = 0; j < 8; j++) {
+                        switch (j) {
+                            case 0:
+                                fields.setField("num." + i + "." + j, stm.getStmdat());
+                                break;
+                            case 1:
+                                String[] strFurinf = stm.getFurinf().split(" ");
+                                String furinf2 = ((strFurinf.length > 1) ? strFurinf[1] : "");//凭证号
+                                fields.setField("num." + i + "." + j, furinf2);
+                                break;
+                            case 2:
+                                fields.setField("num." + i + "." + j, stm.getThrref());//摘要
+                                break;
+                            case 3:
+                                /*String spaDtxnamt = "";
+                                if (!"".equals(stm.getBoramt())){
+                                    spaDtxnamt = df0.format(stm.getBoramt());//借方发生额
+                                }*/
+                                fields.setField("num." + i + "." + j, stm.getBoramt());
+                                break;
+                            case 4:
+                                /*String spaCtxnamt = "";
+                                if (!"".equals(stm.getLenamt())){
+                                    spaCtxnamt= df0.format(stm.getLenamt());//贷方发生额
+                                }*/
+                                fields.setField("num." + i + "." + j, stm.getLenamt());
+                                break;
+                            case 5:
+                                String spaActbal = df0.format(stm.getActbal());
+                                fields.setField("num." + i + "." + j, spaActbal);    //余额
+                                break;
+                            case 6:
+                                String daynum = String.valueOf(stm.getDaynum());
+                                fields.setField("num." + i + "." + j, daynum);      //天数
+                                break;
+                            case 7:
+                                String craccm = df0.format(stm.getCraccm());     //积数
+                                fields.setField("num." + i + "." + j, craccm);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    i++;
+                }
+                ps.setFormFlattening(true);
+                ps.close();
+            }
+            document.open();
+            PdfImportedPage impPage = null;
+            for (int i = 0; i < page; i++) {
+                impPage = pdfCopy.getImportedPage(new PdfReader(baos[i]
+                        .toByteArray()), 1);
+                pdfCopy.addPage(impPage);
+            }
+            pdfCopy.addJavaScript("this.print({bUI: false,bSilent: true,bShrinkToFit: false});" + "\r\nthis.closeDoc();");
+            document.close();
+            resp.reset();
+            ServletOutputStream out = resp.getOutputStream();
+            resp.setContentType("application/pdf");
+            resp.setHeader("Content-disposition", "inline");
+            resp.setContentLength(bao.size());
+            resp.setHeader("Cache-Control", "max-age=30");
+            bao.writeTo(out);
             out.flush();
             out.close();
             ctx.responseComplete();
